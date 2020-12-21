4ebef8b1d4be084a57c05b78e0cd6227e3532f5f
Mathias Nonohay <mathias12300@gmail.com>
2020-12-21 17:01:07 -0300

    primeiro commit

---
+++ Encodable/Model/Viagem.swift
@@ -0,0 +1,25 @@
+//
+//  Viagem.swift
+//  Encodable
+//
+//  Created by Mathias Almeida Nonohay on 12/21/20.
+//  Copyright © 2020 Mathias Almeida Nonohay. All rights reserved.
+//
+
+import Foundation
+
+class Viagem{
+    
+    // MARK: - Atributos
+    
+    let id: Int
+    let titulo: String
+    let quantidadeDeDias: Int
+    let preco: String
+    let localizacao: String
+    
+    //MARK: - Init (Construtor)
+    
+    init(id: Int, _ titulo: String, _ quantidadeDeDias: Int, _ preco: String, _ localizacao: String)
+    
+}
